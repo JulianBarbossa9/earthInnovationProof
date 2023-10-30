1e2e3d73fb39ff21f5ab16e15b891c5f65f2344a
Julian Barbosa <julianbarbossa@hotmail.com>
2023-10-30 15:52:00 -0500

    create data to show all user

---
+++ data.js
@@ -0,0 +1,62 @@
+export const data = [
+  {
+    "Name": "John Doe",
+    "Age": 28,
+    "Email": "john@example.com",
+    "Country": "USA"
+  },
+  {
+    "Name": "Jane Smith",
+    "Age": 35,
+    "Email": "jane@example.com",
+    "Country": "Canada"
+  },
+  {
+    "Name": "Alice Johnson",
+    "Age": 22,
+    "Email": "alice@example.com",
+    "Country": "UK"
+  },
+  {
+    "Name": "Valentina Giraldo",
+    "Age": 25,
+    "Email": "valentina@example.com",
+    "Country": "Argentina"
+  },
+  {
+    "Name": "Juan Perez",
+    "Age": 55,
+    "Email": "juanP@example.com",
+    "Country": "Colombia"
+  },
+  {
+    "Name": "Flor Martinez",
+    "Age": 44,
+    "Email": "flor@example.com",
+    "Country": "Brazil"
+  },
+  {
+    "Name": "Diego Garcia",
+    "Age": 25,
+    "Email": "diego@example.com",
+    "Country": "Argentina"
+  },
+  {
+    "Name": "Ana Ramirez",
+    "Age": 60,
+    "Email": "ana@example.com",
+    "Country": "Ecuador"
+  },
+  {
+    "Name": "Luis Calderon",
+    "Age": 55,
+    "Email": "luis@example.com",
+    "Country": "Colombia"
+  },
+  {
+    "Name": "Maria Restrepo",
+    "Age": 45,
+    "Email": "maria@example.com",
+    "Country": "Colombia"
+  },
+]
